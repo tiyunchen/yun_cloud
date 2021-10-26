84e7e22a07e66a8d628ac776372a6cb354cd0997
chentiyun <15659263357cty>
2021-10-26 11:15:42 +0800

    feat: 项目结构优化并且添加eslint

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose')
-const User = require('../models/user')
-function connect(){
-  const mongoDBurl = 'mongodb://localhost:27017/yun_cloud'
+const mongoose = require('mongoose');
+const User = require('../models/user');
+
+function connect() {
+  const mongoDBurl = 'mongodb://localhost:27017/yun_cloud';
   mongoose.Promise = global.Promise;
-  mongoose.connect(mongoDBurl, {useNewUrlParser: true, useUnifiedTopology: true}).then(res => {
-    console.log(`数据库链接成功`, mongoDBurl)
-  }).catch(err => {
-    console.log('数据库链接失败', err)
-  })
-  const db = mongoose.connection
+  mongoose.connect(mongoDBurl, {useNewUrlParser: true, useUnifiedTopology: true}).then((res) => {
+    console.log('数据库链接成功', mongoDBurl);
+  }).catch((err) => {
+    console.log('数据库链接失败', err);
+  });
+  const db = mongoose.connection;
   // const userModel = new User()
   // userModel.update({username: 'chentyun',email: '949502498@qq.com'})
   // db.on('error', console.error.bind(console, 'connection error:'));
@@ -18,7 +19,6 @@ function connect(){
   // });
 }
 
-
 module.exports = {
-    connect
-}
+  connect,
+};

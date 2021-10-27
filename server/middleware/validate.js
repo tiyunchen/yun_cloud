84595e797f0cd2e4fe24f493d65e01746d18f4b2
chentiyun <15659263357cty>
2021-10-27 11:11:10 +0800

    :art: style: 格式化

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -1,7 +1,7 @@
-const {validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
 
 module.exports = (req, res, next) => {
-  const {errors} = validationResult(req);
+  const { errors } = validationResult(req);
   if (errors && errors.length > 0) {
     res.status(500).send(errors);
   } else {
